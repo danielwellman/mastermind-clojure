8482ab384ee759884f1d1206f6b40720a011a004
Daniel Wellman <dan@danielwellman.com>
2016-01-01 17:36:08 -0500

    Another test of color-and-position-matches for no matches

--- test/mastermind/core_test.clj
+++ test/mastermind/core_test.clj
@@ -3,4 +3,6 @@
             [mastermind.core :refer :all]))
 
 (deftest color-and-position-matches-exact-matches-only
-	(is (= 0 (color-and-position-matches [1 1 1 1] [2 3 4 5]))))
+	(is (= 0 (color-and-position-matches [1 1 1 1] [2 3 4 5])) "Different numbers entirely")
+  (is (= 0 (color-and-position-matches [1 2 3 4] [3 4 2 1])) "Same numbers but wrong positions")
+)
